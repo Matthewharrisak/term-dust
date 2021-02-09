ba03ffe4d1bdc0a421a56380167e9c8e4815add5
Matthew Harris <matthewharrisak@gmail.com>
2021-02-09 13:48:19 -0600

    built navbar and added bootstrap

--- term-dust/src/App.js
+++ term-dust/src/App.js
@@ -1,18 +1,18 @@
+import NavBar from "./Navbar/NavBar";
 import "./App.css";
+import { Container, Row, Col } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code>
-          and save to reload.
-        </p>
-        <a>
-          Delete all this shit and make a react money making application wow!!
-          :)
-        </a>
-      </header>
+    <div>
+    <Container>
+      <Row >
+        <Col>
+          {" "}
+          <NavBar />
+        </Col>
+      </Row>
+    </Container>
     </div>
   );
 }
