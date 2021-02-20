24c60726d1954754be500d2d5eeb99a280f26506
Emma Song Fisher <emmasongf@gmail.com>
2021-02-19 22:07:20 -0600

    video background test

--- term-dust/src/Components/Landing/Landing.js
+++ term-dust/src/Components/Landing/Landing.js
@@ -1,7 +1,8 @@
 import React from "react";
 import styled from "styled-components";
-
 import Wrapper from "../Wrapper/Wrapper";
+// import Video from "./Video";
+
 
 const LandingStyled = styled.div`
   background-color: rgb(229, 159, 70);
@@ -14,13 +15,18 @@ const LandingStyled = styled.div`
 
 export default function About() {
   return (
+    <>
     <LandingStyled id="landing">
       <Wrapper>
         <h1>LANDING</h1>
         <p>
           THIS IS THE LANDING PAGE, HOME OF A VIDEO BACKGROUND IN THE FUTURE
         </p>
+        
       </Wrapper>
+      
     </LandingStyled>
+    {/* <Video/> */}
+    </>
   );
 }
